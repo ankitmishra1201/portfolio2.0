4ba178ff96d7fdbd74c7abdcb674b93a476051d5
Ankit Mishra <ankitmishra@Ankits-MacBook-Pro-2.local>
2023-07-08 12:39:19 +0530

    hello

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,30 @@
 import './App.css';
 import { Landing } from './Landing/landing.tsx';
 import { ParallaxBanner, ParallaxProvider } from 'react-scroll-parallax';
+import {React,useState,useEffect} from 'react';
 
 function App() {
+  const [isLoading, setLoading] = useState(true);
+
+  function someRequest() { //Simulates a request; makes a "promise" that'll run for 2.5 seconds
+    return new Promise(resolve => setTimeout(() => resolve(), 2500));
+  } 
+
+  useEffect(() => {
+    someRequest().then(() => {
+      const loaderElement = document.querySelector(".loader-container");
+      if (loaderElement) {
+        loaderElement.remove();
+        setLoading(!isLoading);
+      }
+    });
+  });
+
+  if (isLoading) {
+    return null;
+  }
+
+
   return (
     <div className="App">
       <ParallaxProvider>
